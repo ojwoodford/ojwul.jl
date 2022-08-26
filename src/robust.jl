e754afd27e8c9aec39f14c5300cc213895cc924a
Oliver J. Woodford <o.j.woodford.98@cantab.net>
2022-08-26 21:42:50 +0100

    Add robustifiers

--- src/robust.jl
+++ src/robust.jl
@@ -1,17 +1,66 @@
-export robustify!
+export Robustifier, NoRobust, HuberKernel, GemanMcclureKernel
+export robustify, robustifyarray
+import Plots.plot
 
-struct HuberKernel{T<:Real}
-    width::T
-    width_squared::T
+# Robustification
+abstract type Robustifier end
+
+function robustifyarray(kernel::Robustifier, cost::AbstractArray)
+    weight = similar(cost)
+    robcost = similar(cost)
+    for ind in eachindex(cost)
+        robcost[ind], weight[ind] = robustify(kernel, cost[ind])
+    end
+    return robcost, weight
 end
 
-HuberKernel(x) = HuberKernel(x, x*x)
 
-function robustify!(cost::T, kernel::HuberKernel{T}) where T<:AbstractFloat
-grad = ones(T, size(cost))
-outliers = cost .> kernel.width_squared
-sqrt_outliers = sqrt.(cost[outliers])
-cost[outliers] .= sqrt_outliers .* (kernel.width * 2) .- kernel.width_squared
-grad[outliers] .= kernel.width ./ sqrt_outliers
-return grad
+struct NoRobust{T<:Real} <: Robustifier
+    height::T
+    height_sqrt::T
+end
+
+NoRobust(h) = HuberKernel(h, sqrt(h))
+
+function robustify(kernel::NoRobust, cost::Number)
+    return cost * kernel.height, kernel.height_sqrt
+end
+
+
+struct HuberKernel{T<:Real} <: Robustifier
+    width::T
+    width_squared::T
+    height::T
+    height_sqrt::T
+end
+
+HuberKernel(w, h) = HuberKernel(w, w*w, h, sqrt(h))
+
+function robustify(kernel::HuberKernel, cost::Number)
+    if cost < kernel.width_squared
+        return cost * kernel.height, kernel.height_sqrt
+    end
+    sqrtcost = sqrt(cost)
+    return (sqrtcost * (kernel.width * 2) - kernel.width_squared) * kernel.height, kernel.width * kernel.height_sqrt / sqrtcost
+end
+
+
+struct GemanMcclureKernel{T<:Real} <: Robustifier
+    width_squared::T
+    height::T
+    height_sqrt::T
+end
+
+GemanMcclureKernel(w, h) = GemanMcclureKernel(w*w, h/(w*w), sqrt(h)/w)
+
+function robustify(kernel::GemanMcclureKernel, cost::Number)
+    w = kernel.width_squared / (cost + kernel.width_squared)
+    return cost * w * kernel.height, w * w * kernel.height_sqrt
+end
+
+
+function displaykernel(kernel, maxval=1)
+    x = LinRange(0, maxval, 1000)
+    cost, weight = robustifyarray(kernel, x .^ 2)
+    plot(x, [cost, weight])
 end
